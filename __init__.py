6c28613934778db27306a63ad6f8026fee94f068
Rohan S Byrne <rohan.byrne@gmail.com>
2019-12-04 16:33:40 +1100

    Unhashed some things that were causing Anaconda problems.

--- __init__.py
+++ __init__.py
@@ -1,9 +1,9 @@
-# from . import built
-# Built = built.Built
-#
-# from . import value
+from . import built
+Built = built.Built
+
+from . import value
 # from . import examples
 # from . import disk
 # from . import paths
-# from . import window
+from . import window
 # from . import wordhash as _wordhash
