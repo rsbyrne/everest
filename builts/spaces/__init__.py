75f87fac905f59a54832ec27aa9d6ea344fdfc85
Rohan S Byrne <rohan.byrne@gmail.com>
2020-02-04 20:33:08 +1100

    Ditto.

--- builts/spaces/__init__.py
+++ builts/spaces/__init__.py
@@ -1,12 +1,15 @@
 from .. import Built
+from ..vector import Vector
 
 class Space(Built):
     def __init__(
             self,
             slice_vector_fn,
+
             **kwargs
             ):
         self._slice_vector_fn = slice_vector_fn
         super().__init__(**kwargs)
     def __getitem__(self, arg):
-        return self._slice_vector_fn(arg)
+        out = self._slice_vector_fn(arg)
+        if isinstance(out, Vector):
