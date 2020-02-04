15dcf177c77e8f4a3f8f9f15745ec9e57b5885a6
Rohan S Byrne <rohan.byrne@gmail.com>
2020-02-04 15:41:10 +1100

    Added some attributes to vector.

--- builts/_vector.py
+++ builts/_vector.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from . import Built
 
 class Vector(Built):
@@ -5,6 +7,7 @@ class Vector(Built):
     def __init__(self, **kwargs):
         self.keys = sorted(kwargs.keys())
         self.vals = [kwargs[key] for key in self.keys]
+        self.data = np.array(self.vals)
         super().__init__()
     def __iter__(self):
         for key, inp in sorted(self.inputs.items()):
