289cfb936a47cd21727b86c973bf15bb57b8ac90
Rohan Byrne <rsbyrne@6250d-144245-l.mobility.unimelb.net.au>
2020-02-05 17:51:19 +1100

    Fixed Enactor

--- builts/_enactor.py
+++ builts/_enactor.py
@@ -3,7 +3,7 @@ from ._cycler import Cycler
 class Enactor(Cycler):
     from .enactor import __file__ as _file_
     def __init__(self, cycler, condition, **kwargs):
-        self.cycler, self.condition = cycler, conditon
+        self.cycler, self.condition = cycler, condition
         super().__init__(**kwargs)
         self._cycle_fns.append(self._enactor_cycleFn)
     def _enactor_cycleFn(self):
