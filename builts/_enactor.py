a9587189c9ca18940a1fe07fa78b6e19fa6477c1
Rohan S Byrne <rohan.byrne@gmail.com>
2020-02-07 11:09:23 +1100

    Tweaks.

--- builts/_enactor.py
+++ builts/_enactor.py
@@ -2,10 +2,13 @@ from ._cycler import Cycler
 
 class Enactor(Cycler):
     from .enactor import __file__ as _file_
-    def __init__(self, cycler, condition, **kwargs):
-        self.cycler, self.condition = cycler, condition
+    def __init__(self,
+            callable = None,
+            condition = None,
+            **kwargs
+            ):
+        self.callable, self.condition = callable, condition
         super().__init__(**kwargs)
         self._cycle_fns.append(self._enactor_cycleFn)
     def _enactor_cycleFn(self):
-        if self.condition:
-            self.cycler()
+        if self.condition: self.callable()
