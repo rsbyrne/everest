ed260216f947f15bfe0d11a49fe3f533d30ca817
Rohan Byrne <rsbyrne@6250d-144245-l.mobility.unimelb.net.au>
2020-02-11 18:08:04 +1100

    Tweaked run.sh - it works!

--- linux/run.sh
+++ linux/run.sh
@@ -1,3 +1,10 @@
 SCRIPT=$1
 N=$2
-for i in $(seq 1 $N); do python $SCRIPT; done
+LOGSDIR="./logs"
+OUTFILE=$LOGSDIR"/"$SCRIPT".out"
+ERRORFILE=$LOGSDIR"/"$SCRIPT".error"
+touch $OUTFILE
+touch $ERRORFILE
+for i in $(seq 1 $N)
+do python $SCRIPT $OUTFILE 2> $ERRORFILE &
+done
