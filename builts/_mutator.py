34982b7c5b8a4555f3c28aefe30d22cebaadec36
Rohan S Byrne <rohan.byrne@gmail.com>
2020-02-12 17:38:20 +1100

    Removed diskbased from Mutator.

--- builts/_mutator.py
+++ builts/_mutator.py
@@ -1,11 +1,7 @@
-from types import FunctionType
-
 from . import Built
-from ._diskbased import DiskBased
-from ..writer import Writer
 from ..weaklist import WeakList
 
-class Mutator(DiskBased):
+class Mutator(Built):
     def __init__(
             self,
             **kwargs
@@ -14,5 +10,6 @@ class Mutator(DiskBased):
         self._update_mutateDict_fns = WeakList()
         super().__init__(**kwargs)
     def mutate(self):
+        self._check_anchored()
         for fn in self._update_mutateDict_fns: fn()
         self.writer.add(self._mutateDict, self.hashID)
