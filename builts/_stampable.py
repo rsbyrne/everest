9a7fa980852be7ac03c016e781429303b9785b4a
Rohan S Byrne <rohan.byrne@gmail.com>
2020-02-29 23:25:20 +1100

    Corrected oversight that caused stamps to not update from disk.

--- builts/_stampable.py
+++ builts/_stampable.py
@@ -1,9 +1,9 @@
 import ast
 
-from ._mutator import Mutator
+from ._producer import Producer
 from ._inquirer import Inquirer
 
-class Stampable(Mutator):
+class Stampable(Producer):
 
     def __init__(
         self,
@@ -11,30 +11,22 @@ class Stampable(Mutator):
         ):
 
         self.stamps = [(self.hashID, 0),]
-        self.localObjects['stamps'] = self.stamps
 
         super().__init__(**kwargs)
 
-        # Mutator attributes:
-        self._update_mutateDict_fns.append(self._stampable_update_mutateDict)
+        # Producer attributes:
+        self._post_save_fns.append(self._stampable_update)
 
         # Built attributes:
-        self._post_anchor_fns.append(self._stampable_update_stamps)
-
-    def _stampable_update_stamps(self):
-        loaded = self.reader(self.hashID, 'stamps')
-        self.stamps = sorted(set(self.stamps))
-
-    def _stampable_update_mutateDict(self):
-        self._mutateDict['stamps'] = self.stamps
+        self._post_anchor_fns.append(self._stampable_update)
 
     def stamp(self, stamper):
         if not isinstance(stamper, Inquirer):
             raise TypeError("Input must be Inquirer class.")
         self.stamps.append((stamper.hashID, self.count()))
+        self.stamps = sorted(set(self.stamps))
 
-    def make_stampsDict(self):
-        stampsDict = {key: [] for key, val in self.stamps}
-        fn = lambda x, y: stampsDict[x].append(y)
-        for key, val in stampsList: fn(key, val)
-        return stampsDict
+    def _stampable_update(self):
+        loaded = self.reader(self.hashID, 'stamps')
+        self.stamps = sorted(set([*self.stamps, *loaded]))
+        self.writer.add(self.stamps, 'stamps')
