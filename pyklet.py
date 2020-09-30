0396c49d4224eae48d4c6c8942a77605bfcc0f11
rohanbyrne <rohanbyrne@penguin>
2020-09-30 21:53:01 +1000

    Added input processing to pyklets.

--- pyklet.py
+++ pyklet.py
@@ -1,10 +1,23 @@
 import inspect
 import pickle
 
+# from .builts import Built, Meta # <- causes circular reference
+# from .globevars import _BUILTTAG_, _CLASSTAG_
+
+from .exceptions import NotYetImplemented
+
 class Pyklet:
     _TAG_ = '_pyklet_'
     def __init__(self, *args, **kwargs):
         self._source = inspect.getsource(self.__class__)
+        args = [
+            self._process_inp(arg)
+            for arg in args
+            ]
+        kwargs = {
+            process_inp(k): process_inp(v)
+                for k, v in sorted(kwargs.items())
+            }
         self._hashObjects = (args, kwargs, self._source)
         self._pickleClass = pickle.dumps(self.__class__)
         self._args, self._kwargs = args, kwargs
@@ -13,3 +26,15 @@ class Pyklet:
     @classmethod
     def _unpickle(cls, args, kwargs):
         return cls(*args, **kwargs)
+    @staticmethod
+    def _process_input(inp):
+        if type(inp) is Meta:
+            raise NotYetImplemented
+            # return _CLASSTAG_ + inp.script
+        elif isinstance(inp, Built):
+            raise NotYetImplemented
+            # return _BUILTTAG_ + inp.hashID
+        else:
+            return inp
+    def _unprocess_input(inp):
+        return inp
