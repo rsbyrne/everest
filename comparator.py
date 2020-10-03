adc0ecf7f1354c037cc918b45d4511bfa37c92be
rohanbyrne <rohanbyrne@penguin>
2020-10-03 17:38:29 +1000

    Improved Comparator implementation.

--- comparator.py
+++ comparator.py
@@ -6,15 +6,21 @@ from .pyklet import Pyklet
 class Prop(Pyklet):
 
     def __init__(self,
+            target,
             *props
             ):
 
-        self.props = props
+        self.target, self.props = target, props
+        for prop in props:
+            if not type(prop) is str:
+                raise TypeError
 
-        super().__init__(*props)
+        super().__init__(self.target, self.props)
 
-    def __call__(self, obj):
+    def __call__(self, obj = None):
 
+        if obj is None:
+            obj = self.target
         for prop in self.props:
             obj = getattr(obj, prop)
 
@@ -23,41 +29,41 @@ class Prop(Pyklet):
 class Comparator(Pyklet):
 
     def __init__(self,
-            *args,
+            *terms,
             op : (str, FunctionType) = bool,
             asList = False,
             invert = False
             ):
 
+        terms = [Prop(t[0], *t[1:]) if type(t) is tuple else t for t in terms]
+
         if type(op) is str:
             op = operator.__dict__[op]
 
-        super().__init__(*args, op = op, asList = asList, invert = invert)
+        super().__init__(*terms, op = op, asList = asList, invert = invert)
 
-        self.args, self.op, self.asList, self.invert = \
-            args, op, asList, invert
+        self.terms, self.op, self.asList, self.invert = \
+            terms, op, asList, invert
 
     def __call__(self, *queryArgs):
 
         queryArgs = iter(queryArgs)
-        args = []
-        for arg in self.args:
-            if arg is None:
-                args.append(next(queryArgs))
-            elif type(arg) is Prop:
-                args.append(arg(next(queryArgs)))
-            else:
-                args.append(arg)
-        try:
-            next(queryArgs)
-            raise ValueError("Leftover args.")
-        except StopIteration:
-            pass
+        terms = []
+        for t in self.terms:
+            if type(t) is Prop:
+                if t.target is None:
+                    t = t(next(queryArgs))
+                else:
+                    t = t()
+            elif t is None:
+                t = next(queryArgs)
+            terms.append(t)
+        terms.extend(list(queryArgs))
 
         if self.asList:
-            out = bool(self.op(args))
+            out = bool(self.op(terms))
         else:
-            out = bool(self.op(*args))
+            out = bool(self.op(*terms))
         if self.invert:
             out = not out
 
@@ -66,16 +72,17 @@ class Comparator(Pyklet):
     def __bool__(self):
         return bool(self())
 
-    def close(self, *queryArgs):
-
-        return Nullary(lambda: self(*queryArgs))
-
-class Nullary:
-
-    def __init__(self, fn):
-
-        self.fn = fn
-
-    def __bool__(self):
-
-        return bool(self.fn())
+#     def close(self, *queryArgs):
+#
+#         return Nullary(self, queryArgs)
+#
+# class Evaluator(Pyklet):
+#
+#     def __init__(self, comparator, queryArgs):
+#
+#         self.comparator, self.queryArgs = comparator, queryArgs
+#         super().__init__(comparator, queryArgs)
+#
+#     def __bool__(self):
+#
+#         return self.comparator(self.queryArgs)
