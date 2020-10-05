3c1de63f35792b6cdacebe4472a8a1e552bdcb00
rohanbyrne <rohanbyrne@penguin>
2020-10-05 14:04:33 +1100

    Integrated Mutable into class hierarchy.

--- builts/_mutable.py
+++ builts/_mutable.py
@@ -1,11 +1,27 @@
+from collections import OrderedDict
+
 from . import Built
+
+from . import BuiltException, MissingMethod, MissingAttribute, MissingKwarg
+class MutableException(BuiltException):
+    pass
+class MutableMissingMethod(MissingMethod, MutableException):
+    pass
+class MutableMissingAttribute(MissingAttribute, MutableException):
+    pass
+class MutableMissingKwarg(MissingKwarg, MutableException):
+    pass
 
 class Mutable(Built):
 
     def __init__(self,
+            _mutableKeys = None,
             **kwargs
             ):
 
-        self.mutables = dict()
+        if _mutableKeys is None:
+            raise MutableMissingKwarg
+
+        self.mutables = OrderedDict([(k, None) for k in _mutableKeys])
 
         super().__init__(**kwargs)
