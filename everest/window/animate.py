1cf97c770dbc49250a7bc39022153a9ca3b6d1ef
Ubuntu <ubuntu@pluto1.novalocal>
2021-04-21 08:09:49 +0000

    Further improvements to animate.

--- everest/window/animate.py
+++ everest/window/animate.py
@@ -6,7 +6,6 @@ import subprocess
 from subprocess import PIPE
 import shutil
 import glob
-
 import numpy as np
 
 from everest import disk
@@ -25,11 +24,17 @@ def frame_iterate(frames):
         for frame in frames:
             yield frame, 1.
 
-def process_durations(rawdurations, duration):
+def process_durations(rawdurations, duration = None):
+    if duration is None:
+        duration = len(rawdurations) / 25.
     rawdurations = np.array(rawdurations)
-    weights = rawdurations / rawdurations.max()
-    weights[0] = 1.
-    return weights * duration / len(weights)   
+    assert len(rawdurations) > 1
+    rawdurations[0] = rawdurations[1]
+    weights = rawdurations / rawdurations.sum()
+    assert np.allclose(weights.sum(), 1), weights.sum()
+    durations = weights * duration
+    assert abs(sum(durations) - duration) < 1e-6, abs(sum(durations) - duration)
+    return durations
 
 def get_ffconcat(filedir, items, durations):
     randname = disk.tempname(_mpiignore_ = True)
@@ -58,7 +63,7 @@ def get_tempDir(outputPath):
     cleanup = lambda: shutil.rmtree(tempDir, ignore_errors = True)
     cleanup()
     os.makedirs(tempDir)
-    return tempDir
+    return tempDir, cleanup
 
 def process_args(source, duration, outputPath):
     if isinstance(source, str):
@@ -70,32 +75,39 @@ def process_args(source, duration, outputPath):
         mode = 'sav'
     if mode == 'mov':
         infile = source
-        pts = duration / get_length(infile)
+        if duration is None:
+            pts = 1
+        else:
+            pts = duration / get_length(infile)
         cleanup = lambda: None
     else:
         pts = 1
         if mode == 'dir':
-            loadDir = source
+            fileDir = source
             rawdurations = None
         else:
-            loadDir = get_tempDir(outputPath)
-            frames, rawdurations = frame_iterate(source)
-            for i, frame in enumerate(frames):
-                frame.save(name, path = tempDir, add = i)
-        searchpath = glob.glob(os.path.join(loadDir, '*.png'))
+            fileDir, cleanup = get_tempDir(outputPath)
+            rawdurations = []
+            for i, (frame, rawdur) in enumerate(frame_iterate(source)):
+                dest = 'in_' + str(i).zfill(8)
+                if isinstance(frame, str):
+                    os.symlink(frame, os.path.join(fileDir, dest + '.png'))
+                else:
+                    frame.save(dest, path = fileDir)
+                rawdurations.append(rawdur)
+        searchpath = glob.glob(os.path.join(fileDir, '*.png'))
         items = sorted(os.path.basename(path) for path in searchpath)
         if rawdurations is None:
             rawdurations = [0., *[1. for _ in items]]
         durations = process_durations(rawdurations, duration)
-        assert abs(sum(durations) - duration) < 1e-6
-        infile = get_ffconcat(loadDir, items, durations)
+        infile = get_ffconcat(fileDir, items, durations)
         if mode == 'dir':
             cleanup = lambda: os.remove(infile)
     return infile, pts, cleanup
 
 def animate(
         source,
-        duration,
+        duration = None,
         name = None,
         outputPath = '.',
         overwrite = False,
@@ -130,7 +142,10 @@ def animate(
             ])
         completed = subprocess.run(
             cmd,
-            stdout = PIPE, stderr = PIPE, shell = True, check = True,
+            stdout = PIPE,
+            stderr = PIPE,
+            shell = True,
+            check = True,
             )
 
     finally:
