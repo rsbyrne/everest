0ee8610c211e066c4a811215d0712f4f0fef1ff0
rsbyrne <rohan.byrne@gmail.com>
2021-05-24 16:32:49 +1000

    Improved image concatenation.

--- everest/window/imop.py
+++ everest/window/imop.py
@@ -9,6 +9,21 @@ from .image import Image as _Image
 class ImOp(_Image):
     ...
 
+def rescale_width(image, targetwidth, **kwargs):
+    width, height = image.width, image.height
+    if width == targetwidth:
+        return image
+    scalefactor = targetwidth / width
+    newlength = round(scalefactor * height)
+    return image.resize((targetwidth, newlength), **kwargs)
+def rescale_height(image, targetheight, **kwargs):
+    width, height = image.width, image.height
+    if height == targetheight:
+        return image
+    scalefactor = targetheight / height
+    newlength = round(scalefactor * width)
+    return image.resize((newlength, targetheight), **kwargs)
+
 class Concat(ImOp):
     __slots__ = ('horiz', 'terms',)
     def __init__(self, *terms, horiz = True, **kwargs):
@@ -17,25 +32,22 @@ class Concat(ImOp):
         super().__init__(*terms, **kwargs)
     def get_pilimg(self):
         images = tuple(im.pilimg for im in self.terms)
-            # (im.pilimg if hasattr(im, 'pilimg') else im) for im in self.terms
-            # )
         horiz = self.horiz
-        keys = 'width', 'height'
-        longkey, shortkey = keys if horiz else keys[::-1]
-        dims = [
-            max(getattr(im, shortkey) for im in images),
-            sum(getattr(im, longkey) for im in images),
-            ]
-        if horiz:
-            dims.reverse()
-        out = _PILImage.new('RGB', dims, **self.pilkwargs)
+        longkey, shortkey = \
+            ('width', 'height') if horiz else ('height', 'width')
+        shortlen = min(getattr(im, shortkey) for im in images)
+        targetlength = min(getattr(im, shortkey) for im in images)
+        rescaler = rescale_height if horiz else rescale_width
+        images = tuple(rescaler(im, targetlength) for im in images)
+        assert len(set(getattr(im, shortkey) for im in images)) == 1
+        longlen = sum(getattr(im, longkey) for im in images)
+        outdims = (longlen, shortlen) if horiz else (shortlen, longlen)
+        out = _PILImage.new('RGB', outdims, **self.pilkwargs)
         pos = 0
-        for im in images:
-            dims = [0, pos]
-            if horiz:
-                dims.reverse()
-            out.paste(im, dims)
-            pos += getattr(im, longkey)
+        for image in images:
+            pastedims =  (pos, 0) if horiz else (0, pos)
+            out.paste(image, pastedims)
+            pos += getattr(image, longkey)
         return out
 
 def hstack(*terms, **kwargs):
