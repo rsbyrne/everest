f8c0a60ef5e50ccbf017ef701a9f37b0a63a44c9
rsbyrne <rohan.byrne@gmail.com>
2021-10-02 07:06:02 +0000

    Quick push

--- push.sh
+++ push.sh
@@ -3,7 +3,7 @@ currentDir=$PWD
 cd "$(dirname "$0")"
 #bash configure_ssh.sh
 eval "$(ssh-agent)" && ssh-add ~/.ssh/*.pem
-bash purge_notebooks
+bash ./purge_notebooks.sh
 git config --global user.email "rohan.byrne@gmail.com"
 git config --global user.name "rsbyrne"
 git add .
